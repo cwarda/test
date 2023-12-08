948f1dfefac2b02c1e6522c24ab72eedc8a8af6b
Carla <carlawarda@gmail.com>
2023-12-07 20:47:29 -0400

    Test

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
+
+function getAllNames() {}
+
+function getConstellationsByQuadrant(quadrant) {}
+
+module.exports = {
+  getAllNames,
+  getConstellationsByQuadrant,
+};
+
+//test
